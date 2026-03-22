5a78e40cef15698748fcf0ef8fc40b6dbaf0d380
litoma <litoma4u@gmail.com>
2026-03-22 21:28:05 +0900

    Revert "feat: hide TradingView widget native header"
    This reverts commit 4a4a7c0dacb08bf35259836d1b3c297c15436478.

--- js/chart.js
+++ js/chart.js
@@ -64,12 +64,7 @@ window.ChartManager = {
         <html>
           <head>
             <style>
-              body, html { margin: 0; padding: 0; width: 100%; height: 100%; overflow: hidden; background: transparent; }
-              .tradingview-widget-container {
-                  position: relative;
-                  top: -46px;
-                  height: calc(100% + 46px);
-              }
+              body, html { margin: 0; padding: 0; width: 100%; height: 100%; overflow: hidden; }
             </style>
           </head>
           <body>
