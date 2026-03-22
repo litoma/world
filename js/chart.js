4a4a7c0dacb08bf35259836d1b3c297c15436478
litoma <litoma4u@gmail.com>
2026-03-22 21:25:26 +0900

    feat: hide TradingView widget native header

--- js/chart.js
+++ js/chart.js
@@ -64,7 +64,12 @@ window.ChartManager = {
         <html>
           <head>
             <style>
-              body, html { margin: 0; padding: 0; width: 100%; height: 100%; overflow: hidden; }
+              body, html { margin: 0; padding: 0; width: 100%; height: 100%; overflow: hidden; background: transparent; }
+              .tradingview-widget-container {
+                  position: relative;
+                  top: -46px;
+                  height: calc(100% + 46px);
+              }
             </style>
           </head>
           <body>
